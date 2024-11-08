c1eb8ff8989541eed89c4fbaccbcb967ab1b2622
rioxiu <rioxiu@riox1u>
2024-11-08 20:55:32 +0700

    fix: change config host on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
+  config.hosts << "testrails.richardosinulingga.site"
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
